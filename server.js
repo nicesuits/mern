7de8343b1d3ed0982b1954e9897abf231e79add9
Raion314 <raion314@outlook.com>
2018-12-29 13:41:17 -0800

    add post endpoint

--- server.js
+++ server.js
@@ -31,6 +31,15 @@ app.get("/api/issues", (req, res) => {
   res.json({ _metadata: metadata, records: issues });
 });
 
+app.post("/api/issues", (req, res) => {
+  const newIssue = req.body;
+  newIssue.id = issues.length + 1;
+  newIssue.created = new Date();
+  if (!newIssue.status) newIssue.status = "New";
+  issues.push(newIssue);
+  res.json(newIssue);
+});
+
 app.listen(3000, () => {
   console.log("App started on port 3000");
 });
