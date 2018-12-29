d916309e605cdd25a2c5182ed0085db20348e1b7
Raion314 <raion314@outlook.com>
2018-12-29 01:22:04 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+const contentNode = document.getElementById("contents");
+
+class IssueFilter extends React.Component {
+  render() {
+    return <div>This is a placeholder for the issue filter.</div>;
+  }
+}
+class IssueTable extends React.Component {
+  render() {
+    return <div>This is a placeholder for the issue table.</div>;
+  }
+}
+class IssueAdd extends React.Component {
+  render() {
+    return <div>This is a placeholder for the issue add.</div>;
+  }
+}
+class IssueList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Issue Tracker</h1>
+        <IssueFilter />
+        <hr />
+        <IssueTable />
+        <hr />
+        <IssueAdd />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<IssueList />, contentNode);
