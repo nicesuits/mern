8891d4315d5c2e064539a7b5e81a032f759b8ddd
Raion314 <raion314@outlook.com>
2019-01-01 21:02:59 -0800

    added routing to app. Fixing url recognition

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router, Route, Link, HashRouter, Switch } from "react-router-dom";
 
 import IssueList from "./components/IssueList";
+import IssueEdit from "./components/IssueEdit";
 
 const contentNode = document.getElementById("contents");
+const NoMatch = () => <p>Page Not Found</p>;
+const RoutedApp = () => (
+  <HashRouter>
+    <Switch>
+      <Route path="/" component={IssueList} />
+      <Route path="/issueEdit" component={IssueEdit} />
+      <Route path="*" component={NoMatch} />
+    </Switch>
+  </HashRouter>
+);
 
-ReactDOM.render(<IssueList />, contentNode);
+// ReactDOM.render(<IssueList />, contentNode);
+ReactDOM.render(<RoutedApp />, contentNode);
--- static/App.js
+++ static/App.js
@@ -1,8 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router, Route, Link, HashRouter, Switch } from "react-router-dom";
 
 import IssueList from "./components/IssueList";
+import IssueEdit from "./components/IssueEdit";
 
 const contentNode = document.getElementById("contents");
+const NoMatch = () => React.createElement(
+  "p",
+  null,
+  "Page Not Found"
+);
+const RoutedApp = () => React.createElement(
+  HashRouter,
+  null,
+  React.createElement(
+    Switch,
+    null,
+    React.createElement(Route, { path: "/", component: IssueList }),
+    React.createElement(Route, { path: "/issueEdit", component: IssueEdit }),
+    React.createElement(Route, { path: "*", component: NoMatch })
+  )
+);
 
-ReactDOM.render(React.createElement(IssueList, null), contentNode);
+// ReactDOM.render(<IssueList />, contentNode);
+ReactDOM.render(React.createElement(RoutedApp, null), contentNode);
