bf012698a1cc321f9198855fb16f16101f2c7952
Raion314 <raion314@outlook.com>
2019-01-01 20:27:22 -0800

    add issue edit component

---
+++ src/components/IssueEdit.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class IssueEdit extends Component {
+  render() {
+    return <div>This is a placeholder for the Issue Edit page</div>;
+  }
+}
---
+++ static/components/IssueEdit.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class IssueEdit extends Component {
+  render() {
+    return React.createElement(
+      "div",
+      null,
+      "This is a placeholder for the Issue Edit page"
+    );
+  }
+}
