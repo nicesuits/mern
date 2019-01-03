f7f6f300b9f5ea6487e8fdab4e146c84441fb02c
Raion314 <raion314@outlook.com>
2019-01-02 19:18:24 -0800

    add filter

--- src/components/IssueFilter.jsx
+++ src/components/IssueFilter.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default class IssueFilter extends React.Component {
   render() {
+    const Separator = () => <span> | </span>;
     return (
       <div>
-        This is a placeholder for the issue filter. From the IssueFilter
-        Component
+        <Link to="/issues">All Issues</Link>
+        <Separator />
+        <Link to="/issues?status=Open">Open Issues</Link>
+        <Separator />
+        <Link to="/issues?status=Assigned">Assigned Issues</Link>
       </div>
     );
   }
--- static/components/IssueFilter.js
+++ static/components/IssueFilter.js
@@ -1,11 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default class IssueFilter extends React.Component {
   render() {
+    const Separator = () => React.createElement(
+      "span",
+      null,
+      " | "
+    );
     return React.createElement(
       "div",
       null,
-      "This is a placeholder for the issue filter. From the IssueFilter Component"
+      React.createElement(
+        Link,
+        { to: "/issues" },
+        "All Issues"
+      ),
+      React.createElement(Separator, null),
+      React.createElement(
+        Link,
+        { to: "/issues?status=Open" },
+        "Open Issues"
+      ),
+      React.createElement(Separator, null),
+      React.createElement(
+        Link,
+        { to: "/issues?status=Assigned" },
+        "Assigned Issues"
+      )
     );
   }
 }
