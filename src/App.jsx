1bc4e249485227682b03b14bda066cee6a03f547
Raion314 <raion314@outlook.com>
2019-01-04 23:43:06 -0800

    refactor navbar

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,52 @@ import {
   Switch,
   withRouter
 } from "react-router-dom";
+import {
+  Navbar,
+  Nav,
+  NavItem,
+  NavDropdown,
+  MenuItem,
+  Glyphicon
+} from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 import IssueList from "./components/IssueList";
 import IssueEdit from "./components/IssueEdit";
 
 const contentNode = document.getElementById("contents");
 const NoMatch = () => <p>Page Not Found</p>;
+const Header = () => (
+  <Navbar>
+    <Navbar.Header>
+      <Navbar.Brand>Issue Tracker</Navbar.Brand>
+    </Navbar.Header>
+    <Nav>
+      <LinkContainer to="/issues">
+        <NavItem>Issues</NavItem>
+      </LinkContainer>
+      <LinkContainer to="/reports">
+        <NavItem>Reports</NavItem>
+      </LinkContainer>
+    </Nav>
+    <Nav pullRight>
+      <NavItem>
+        <Glyphicon glyph="plus" />
+        Create Issue
+      </NavItem>
+      <NavDropdown
+        id="user-dropdown"
+        title={<Glyphicon glyph="option-horizontal" />}
+        noCaret
+      >
+        <MenuItem>Logout</MenuItem>
+      </NavDropdown>
+    </Nav>
+  </Navbar>
+);
 const App = () => (
   <div className="container-fluid">
-    <header className="header">
-      <h1>Issue Tracker</h1>
-    </header>
+    <Header />
     <main className="contents">
       <BrowserRouter>
         <Switch>
